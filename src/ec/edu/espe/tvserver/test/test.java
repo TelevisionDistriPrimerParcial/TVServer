74cab5c05adc1d1ae90ad8b80e49a141b052ca5a
ivan16luis <ivanlugiz16@yahoo.com>
2016-12-26 18:41:45 -0500

    cree un pakete tipo test y una clase principal de nombre test selecciono solo lo que cree lo de mas no lo marco

---
+++ src/ec/edu/espe/tvserver/test/test.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.tvserver.test;
+
+/**
+ *
+ * @author Luis
+ */
+public class test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*
+         PRUEBA DE SUBIR CAMBIOS AL REPOSITORIO.
+         SALUDOS
+         */
+    }
+
+}
